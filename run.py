64c3fd8a3add92df90ed6af476dd93b52c8d07c2
Xyey3 <164136905+Xyey3@users.noreply.github.com>
2024-12-11 16:22:00 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import random
 import numpy as np
 
 if __name__ == '__main__':
-    fix_seed = 42
+    fix_seed = 2023
     random.seed(fix_seed)
     torch.manual_seed(fix_seed)
     np.random.seed(fix_seed)
